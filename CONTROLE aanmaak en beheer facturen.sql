8f295a756dad5fd7c3f06681908c0fe2d7b1ef74
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2024-10-29 13:42:17 +0100

    Update CONTROLE aanmaak en beheer facturen.sql

--- CONTROLE aanmaak en beheer facturen.sql
+++ CONTROLE aanmaak en beheer facturen.sql
@@ -9,11 +9,11 @@ FROM	account_invoice i
 	JOIN res_partner p ON i.partner_id = p.id
 	JOIN product_product pp ON p.membership_renewal_product_id = pp.id
 	JOIN res_users u ON i.create_uid = u.id
-WHERE 	i.create_date > '2024-10-10 08:31:00.00000' -- 9913
+WHERE 	i.create_date > '2024-10-21 12:07:53.000000' -- 9913
 	--i.create_date > '2017-11-10 08:00:00.00000' --i.create_date > '2017-11-09 10:49:36.222473' - 77301; i.create_date > '2017-11-09 07:42:19.440333' - 70154; '2017-11-08 07:56:55.00000' - 68802; '2017-11-07 09:58:38.00000' - 55562; '2017-11-07 08:27:13.00000' - 42334; '2017-11-06 09:55:47.00000' - 41872; '2017-11-04 10:19:30.00000' - 38423; 
 	AND i.membership_renewal --AND i.state = 'open'
-	AND i.number LIKE '%LID24%'
-	--AND u.login = 'axel.vandencamp'
+	AND i.number LIKE '%LID24%' 
+	AND u.login = 'axel.vandencamp'
 ORDER BY i.create_date DESC -- ASC -- 
 
 ----------------------------------------------------------
@@ -23,18 +23,17 @@ ORDER BY i.create_date DESC -- ASC --
 
 SELECT p_id, max(aantal) aantal, status  FROM
 (
-	SELECT p.id p_id, p.membership_state status,--u.login, i.create_date, i.id, , i.number
+	SELECT p.id p_id, p.membership_state status, --p.membership_end, p.membership_stop--u.login, i.create_date, i.id, , i.number
 		ROW_NUMBER() OVER(PARTITION BY p.id ORDER BY p.id asc) AS Aantal
 	FROM account_invoice i
 		JOIN res_partner p ON i.partner_id = p.id
 		JOIN product_product pp ON p.membership_renewal_product_id = pp.id
 		JOIN res_users u ON i.create_uid = u.id
-	WHERE 	i.create_date > '2023-10-20 05:00:00.00000' 
+	WHERE 	i.create_date > '2024-10-21 12:07:53.00000' 
 		--AND i.number LIKE '%LID17%' AND i.membership_renewal
 		--AND i.number LIKE 'GIFT/2017%'
 		AND i.state = 'open'
 		AND u.login = 'axel.vandencamp'
-		
 ) x
 WHERE x.aantal > 1
 GROUP BY p_id, status
