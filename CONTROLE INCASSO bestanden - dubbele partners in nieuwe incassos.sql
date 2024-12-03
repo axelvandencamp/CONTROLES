b7be46b218739e9906adca84762e51f2fba032a7
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2024-12-03 17:30:54 +0100

    Update CONTROLE INCASSO bestanden - dubbele partners in nieuwe incassos.sql

--- CONTROLE INCASSO bestanden - dubbele partners in nieuwe incassos.sql
+++ CONTROLE INCASSO bestanden - dubbele partners in nieuwe incassos.sql
@@ -17,7 +17,7 @@ FROM
 		(
 		SELECT pl.id pl_id, *, ROW_NUMBER() OVER(PARTITION BY pl.partner_id ORDER BY pl.partner_id asc) AS Aantal
 		FROM payment_line pl JOIN payment_order po ON pl.order_id = po.id
-		WHERE po.reference IN ('2024/05901','2024/05902','2024/05903','2024/05904','2024/05905','2024/05906','2024/05907')
+		WHERE po.reference IN ('2024/06130','2024/06132','2024/06133')
 		) x
 	WHERE x.aantal > 1
 	) y
@@ -28,10 +28,32 @@ FROM
 	JOIN account_invoice_line ail ON ail.id = aml.invoice_line_id
 	JOIN account_invoice ai ON ai.id = ail.invoice_id
 	JOIN res_partner p ON y.partner_id = p.id
-WHERE po.reference IN ('2024/05901','2024/05902','2024/05903','2024/05904','2024/05905','2024/05906','2024/05907')
-	--AND y.partner_id IN (301143,280859,189240,301142)
+WHERE po.reference IN ('2024/06130','2024/06132','2024/06133')
+	AND y.partner_id IN (20869,16653,16901)
 ORDER BY y.partner_id
 --) z WHERE z.Aantal > 1
+--------------------------------------------------------
+-- status ophalen ahv ID's uit incasso opdrachten met factuur info
+--------------------------------------------------------
+--/*
+SELECT pl.partner_id, p.membership_state, i.number, aml.invoice_line_id, il.id, po.reference, i.amount_total, p.membership_end recentste_einddatum_lidmaatschap
+FROM 	res_partner p
+	JOIN payment_line pl ON p.id = pl.partner_id
+	JOIN payment_order po ON pl.order_id = po.id
+	JOIN account_move_line aml ON aml.id = pl.move_line_id
+	JOIN account_invoice_line il ON il.id = aml.invoice_line_id
+	JOIN account_invoice i ON i.id = il.invoice_id
+
+	JOIN (SELECT partner FROM membership_membership_line ml WHERE ml.state = 'paid' AND ml.date_to >= '2023-12-31') SQ1 ON SQ1.partner = p.id --voor controle december voor volgend jaar
+	
+WHERE po.reference IN ('2024/06130','2024/06132','2024/06133')
+	--AND p.membership_state = 'paid'  --voor controle tijdens het jaar
+	AND p.membership_end = '2025-12-31'
+	--AND NOT(amount_total IN (44,15,27, 10,11,37,38))
+--ORDER BY amount_total ASC	
+ORDER BY po.reference, i.number
+
+--*/
 --------------------------------------------------------
 -- specifiek partner_id opzoeken
 --------------------------------------------------------
@@ -56,28 +78,7 @@ WHERE po.reference IN ('2024/05901','2024/05902','2024/05903','2024/05904','2024
 	--AND NOT(sm.state = 'valid')
 ORDER BY po.reference	
 --*/
---------------------------------------------------------
--- status ophalen ahv ID's uit incasso opdrachten met factuur info
---------------------------------------------------------
---/*
-SELECT pl.partner_id, p.membership_state, i.number, aml.invoice_line_id, il.id, po.reference, i.amount_total, p.membership_end recentste_einddatum_lidmaatschap
-FROM 	res_partner p
-	JOIN payment_line pl ON p.id = pl.partner_id
-	JOIN payment_order po ON pl.order_id = po.id
-	JOIN account_move_line aml ON aml.id = pl.move_line_id
-	JOIN account_invoice_line il ON il.id = aml.invoice_line_id
-	JOIN account_invoice i ON i.id = il.invoice_id
 
-	JOIN (SELECT partner FROM membership_membership_line ml WHERE ml.state = 'paid' AND ml.date_to >= '2023-12-31') SQ1 ON SQ1.partner = p.id --voor controle december voor volgend jaar
-	
-WHERE po.reference IN ('2024/05901','2024/05902','2024/05903','2024/05904','2024/05905','2024/05906','2024/05907')
-	--AND p.membership_state = 'paid'  --voor controle tijdens het jaar
-	AND p.membership_end = '2024-12-31'
-	--AND NOT(amount_total IN (44,15,27, 10,11,37,38))
---ORDER BY amount_total ASC	
-ORDER BY po.reference, i.number
-
---*/
 --------------------------------------------------------
 -- TOTALEN: aantal betalingen en som bedrag in incasso bestand
 --------------------------------------------------------
