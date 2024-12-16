54df4c1f96ddc008393867db6449e3a6f5c20916
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2024-12-16 11:45:10 +0100

    Update CONTROLE - erp test - aanmaak en beheer facturen.sql

--- CONTROLE - erp test - aanmaak en beheer facturen.sql
+++ CONTROLE - erp test - aanmaak en beheer facturen.sql
@@ -3,18 +3,18 @@
 --------------------------------------------------------------------
 SELECT pp.name_template, * 
 SELECT i.partner_id, i.reference ogm, i.amount_total bedrag, i.state, i.create_date, i.internal_number, i.membership_renewal, i.number--, i.*
---SELECT COUNT(DISTINCT p.id) aantal, MAX(i.create_date) date
+--SELECT COUNT(DISTINCT p.id) aantal, MIN(i.create_date) date, MAX(i.create_date), AGE(MAX(i.create_date),MIN(i.create_date))
 FROM	account_invoice i
 	--JOIN account_invoice_line il ON i.id = il.invoice_id
 	JOIN res_partner p ON i.partner_id = p.id
 	JOIN product_product pp ON p.membership_renewal_product_id = pp.id
 	JOIN res_users u ON i.create_uid = u.id
-WHERE i.create_date > '2024-10-16 08:31:00.00000' --	i.create_date BETWEEN '2024-10-21 08:30:00.00000'AND '2024-10-21 09:30:00.00000' -- 
+WHERE i.create_date >= '2024-12-10 08:33:21.119627' --	i.create_date BETWEEN '2024-10-21 08:30:00.00000'AND '2024-10-21 09:30:00.00000' -- 
 	--i.create_date > '2017-11-10 08:00:00.00000' --i.create_date > '2017-11-09 10:49:36.222473' - 77301; i.create_date > '2017-11-09 07:42:19.440333' - 70154; '2017-11-08 07:56:55.00000' - 68802; '2017-11-07 09:58:38.00000' - 55562; '2017-11-07 08:27:13.00000' - 42334; '2017-11-06 09:55:47.00000' - 41872; '2017-11-04 10:19:30.00000' - 38423; 
 	AND i.membership_renewal --AND i.state = 'open'
 	AND i.number LIKE '%LID24%' 
 	AND u.login = 'axel.vandencamp'
-ORDER BY i.create_date DESC -- ASC -- 
+ORDER BY i.create_date ASC -- DESC -- 
 
 ----------------------------------------------------------
 -- eventuele dubbels opsporen (op basis van database ID --
