c19259315e4f3218f017ed6b555ca3c4e417a208
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2025-01-30 16:38:40 +0100

    Update CONTROLE weigering met inactief partner_id opzoeken.sql

--- CONTROLE weigering met inactief partner_id opzoeken.sql
+++ CONTROLE weigering met inactief partner_id opzoeken.sql
@@ -2,5 +2,11 @@ SELECT p.id partner_id, bs.name, p.active, csf.*
 FROM res_partner p 
 	JOIN account_coda_sdd_refused csf ON p.id = csf.partner_id  
 	JOIN account_bank_statement bs ON bs.id = csf.stat_id 
+
+--WHERE p.id IN (280783,394144,358865)
+
 WHERE bs.name = '23-288-335' 
 	AND p.active = 'false'
+
+
+--SELECT * FROM res_users WHERE id = 623	
