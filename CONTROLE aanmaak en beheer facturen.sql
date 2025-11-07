b0909a4cc89408d452f26413eb31bfca4cda8655
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2025-11-07 07:24:18 +0100

    Update CONTROLE aanmaak en beheer facturen.sql

--- CONTROLE aanmaak en beheer facturen.sql
+++ CONTROLE aanmaak en beheer facturen.sql
@@ -9,10 +9,10 @@ FROM	account_invoice i
 	JOIN res_partner p ON i.partner_id = p.id
 	JOIN product_product pp ON p.membership_renewal_product_id = pp.id
 	JOIN res_users u ON i.create_uid = u.id
-WHERE 	i.create_date > '2024-10-21 12:07:53.000000' -- 9913
+WHERE 	i.create_date > '2025-10-21 10:17:20.000000' -- 9913
 	--i.create_date > '2017-11-10 08:00:00.00000' --i.create_date > '2017-11-09 10:49:36.222473' - 77301; i.create_date > '2017-11-09 07:42:19.440333' - 70154; '2017-11-08 07:56:55.00000' - 68802; '2017-11-07 09:58:38.00000' - 55562; '2017-11-07 08:27:13.00000' - 42334; '2017-11-06 09:55:47.00000' - 41872; '2017-11-04 10:19:30.00000' - 38423; 
 	AND i.membership_renewal --AND i.state = 'open'
-	AND i.number LIKE '%LID24%' 
+	AND i.number LIKE '%LID25%' 
 	AND u.login = 'axel.vandencamp'
 ORDER BY i.create_date DESC -- ASC -- 
 
@@ -29,7 +29,8 @@ SELECT p_id, max(aantal) aantal, status  FROM
 		JOIN res_partner p ON i.partner_id = p.id
 		JOIN product_product pp ON p.membership_renewal_product_id = pp.id
 		JOIN res_users u ON i.create_uid = u.id
-	WHERE 	i.create_date > '2024-10-21 12:07:53.00000' 
+	WHERE 	--i.create_date > '2025-10-21 10:17:20.000000' 
+		i.create_date > '2025-10-23 19:00:00.000000' 
 		--AND i.number LIKE '%LID17%' AND i.membership_renewal
 		--AND i.number LIKE 'GIFT/2017%'
 		AND i.state = 'open'
